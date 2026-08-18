dfba1d5454f8ee4752a49aa9dd41dd2c677ffdd7
ParkYoungSeo <givpro22@daum.net>
2026-08-18 17:30:41 +0900

    디렉티브 실습 (v-html, v-text, v-bind)

--- README.md
+++ README.md
@@ -17,3 +17,6 @@ http://localhost:5173
 - 프로젝트 구조 확인 (p58-62): index.html에서 main.js로, main.js에서 App.vue로 이어지는 진입 경로 확인
 - App.vue 구성 (p68-69): 기본 스캐폴딩 화면을 비우고 실습 컴포넌트를 갈아 끼우는 구조로 교체. 실습 파일은 `src/components/practices/` 아래 분류
 - 반응형 데이터 (p70): `SampleOne.vue`에서 일반 변수와 `ref()` 변수를 각각 버튼으로 증가시켜 화면 갱신 차이 비교
+- 텍스트 보간법 (p71): `SampleTwo.vue`에서 보간법 안에 JavaScript 표현식을 넣어 출력
+- v-html, v-text (p74-76): 문자열을 HTML로 해석해 주입하는 v-html과 XSS 노출 확인, v-text 및 보간법과 결과 비교
+- v-bind (p77-81): href, src, disabled 동적 바인딩과 클래스 바인딩, 스타일 바인딩
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup>
 import SampleOne from './components/practices/basic/SampleOne.vue'
+import SampleTwo from './components/practices/basic/SampleTwo.vue'
+import DirectiveVHtml from './components/practices/directive/DirectiveVHtml.vue'
+import DirectiveVHtmlXss from './components/practices/directive/DirectiveVHtmlXss.vue'
+import DirectiveVText from './components/practices/directive/DirectiveVText.vue'
+import DirectiveVBind from './components/practices/directive/DirectiveVBind.vue'
+import DirectiveClassBind from './components/practices/directive/DirectiveClassBind.vue'
+import DirectiveStyleBind from './components/practices/directive/DirectiveStyleBind.vue'
 </script>
 
 <template>
   <div style="padding: 20px">
     <SampleOne />
+    <SampleTwo />
+    <DirectiveVHtml />
+    <DirectiveVHtmlXss />
+    <DirectiveVText />
+    <DirectiveVBind />
+    <DirectiveClassBind />
+    <DirectiveStyleBind />
   </div>
 </template>
